b9bc1666cfd20d6a0c421eaf9cb92da9c4b0b6cd
Mywe4ka <mywe4ka@gmail.com>
2015-05-07 16:33:25 +0000

    add index 9.3.2

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,12 +1,6 @@
 include ApplicationHelper
 
 def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
-end
-
-def valid_signin(user)
   visit signin_path
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
