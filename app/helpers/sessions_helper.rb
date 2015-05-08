34d29440c09cd5ab01ed04bb91a28dd0b5dcf7fc
Mywe4ka <mywe4ka@gmail.com>
2015-05-08 09:00:32 +0000

    add pagination, index tests

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
    def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     @current_user = user
-  end
+   end
 
   def signed_in?
     !current_user.nil?
